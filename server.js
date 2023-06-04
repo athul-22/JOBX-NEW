c1987c2761cbb6ffdf9da593b8b476c30b01b889
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-06-04 16:20:28 +0530

    1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/',(req, res)=>{
 })
 
 const port = process.env.PORT || 5000
-app.listen(port , ()=> {
+app.listen(port,()=> {
     console.log(`Server is running on port ${port}...`);
 })
 
